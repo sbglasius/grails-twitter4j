903cedda7ff1e3804e9b105abc9af2f9c305dde5
Bobby Warner <bobbywarner@gmail.com>
2012-12-13 12:44:42 -0600

    Updated to latest twitter4j and Grails 2.2 RC4

--- grails-app/conf/BuildConfig.groovy
+++ grails-app/conf/BuildConfig.groovy
@@ -15,7 +15,9 @@ grails.project.dependency.resolution = {
 	}
 
 	dependencies {
-        runtime group:"org.twitter4j", name:"twitter4j-core", version:"2.2.3"
+        runtime group:"org.twitter4j", name:"twitter4j-core", version:"3.0.2"
+        runtime group:"org.twitter4j", name:"twitter4j-async", version:"3.0.2"
+        runtime group:"org.twitter4j", name:"twitter4j-stream", version:"3.0.2"
     }
 
 	plugins {
