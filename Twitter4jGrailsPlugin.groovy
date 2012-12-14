28a04d91341808072450f917bc05fc80987cd15c
Soeren Berg Glasius <soeren@glasius.dk>
2012-12-14 10:37:47 +0000

    Updated the Plugin descriptor

--- Twitter4jGrailsPlugin.groovy
+++ Twitter4jGrailsPlugin.groovy
@@ -4,58 +4,65 @@ import org.springframework.context.ApplicationContext
 import org.codehaus.groovy.grails.commons.ConfigurationHolder
 
 class Twitter4jGrailsPlugin {
-	
+
 	Logger log = Logger.getLogger(getClass())
-	
-    def version = "2.2.6"
- 
-    def grailsVersion = "1.3 > *"
 
-    def author = "Soeren Berg Glasius, Arthur Neves"
-    def authorEmail = "soeren@glasius.dk, arthurnn@gmail.com"
-    def title = "Twitter4j for Grails"
-    def description = 'Wraps the Twitter4j API by Groovy delegation (see http://www.twitter4j.org for API documentation and examples). Plugin version follows twitter4j version included.'
+	def version = "3.0.2"
 
-    // URL to the plugin's documentation
-    def documentation = "http://grails.org/plugin/twitter4j"
+	def grailsVersion = "1.3 > *"
 
-    def dependsOn = [:]
-    def pluginExcludes = [
-            "grails-app/domain/**",
-            "grails-app/taglib/**",
-            "grails-app/utilites/**",
-            "grails-app/views/error.gsp",
-            "grails-app/layouts/**",
-            "**/test/**"
+	def author = "Soeren Berg Glasius, Arthur Neves"
+	def authorEmail = "soeren@glasius.dk, arthurnn@gmail.com"
+	def title = "Twitter4j for Grails"
+	def description = 'Wraps the Twitter4j API by Groovy delegation (see http://www.twitter4j.org for API documentation and examples). Plugin version follows twitter4j version included.'
 
-    ]
+	def documentation = "http://grails.org/plugin/twitter4j"
 
-    def doWithWebDescriptor = { xml ->}
+	def license = "APACHE"
 
-    def doWithSpring = {
-	
+	def developers = [
+			[name: "Soeren Berg Glasius", email: "soeren@glasius.dk"],
+			[name: "Arthur Neves", email: "arthurnn@gmail.com"]
+	]
+
+    def issueManagement = [ system: "GIT", url: "https://github.com/sbglasius/grails-twitter4j/issues" ]
+
+    def scm = [ url: "https://github.com/sbglasius/grails-twitter4j.git" ]
+
+	def dependsOn = [:]
+	def pluginExcludes = [
+			"grails-app/domain/**",
+			"grails-app/taglib/**",
+			"grails-app/utilites/**",
+			"grails-app/views/error.gsp",
+			"grails-app/layouts/**",
+			"**/test/**"
+	]
+
+	def doWithWebDescriptor = { xml -> }
+
+	def doWithSpring = {
 		def prop = application.config.twitter.userListenerClass
-		if(prop){
+		if(prop) {
 			def clazz = application.classLoader.loadClass(prop)
 			log.debug "Register twitter user listenet[${clazz}]"
 			twitterUserListener(clazz)
-		}else{
+		} else {
 			log.debug "There is no Tweeter User Listener to register."
 		}
-		
-		twitterStream(TwitterUserStreamFactoryBean){
+
+		twitterStream(TwitterUserStreamFactoryBean) {
 			configuration = ConfigurationHolder.config.twitter.'default'
 		}
-		
 	}
 
-    def doWithDynamicMethods = { ctx ->}
+	def doWithDynamicMethods = { ctx -> }
 
-	def doWithApplicationContext = {applicationContext ->
+	def doWithApplicationContext = { applicationContext ->
 		scheduleStream(applicationContext)
 	}
 
-	def onShutdown = {event ->
+	def onShutdown = { event ->
 		def ctx = event.ctx
 		if(!ctx) {
 			log.error("Application context not found. Cannot execute shutdown code.")
@@ -66,24 +73,20 @@ class Twitter4jGrailsPlugin {
 			twitterStream.shutdown()
 		}
 	}
-	
-    def onChange = { event ->}
 
-    def onConfigChange = { event ->}
-	
-	def scheduleStream = {ctx ->
-		
-		if(ctx.containsBean('twitterUserListener')){
+	def onChange = { event -> }
+
+	def onConfigChange = { event -> }
+
+	def scheduleStream = { ctx ->
+		if(ctx.containsBean('twitterUserListener')) {
 			def twitterUserListener = ctx.getBean('twitterUserListener')
 			def twitterStream = ctx.getBean('twitterStream')
 			twitterStream.addListener(twitterUserListener);
 			// user() method internally creates a thread which manipulates TwitterStream and calls these adequate listener methods continuously.
 			twitterStream.user();
-			
+
 			log.debug "Twitter User Stream Created.."
 		}
-		
-		
-    }
-	
+	}
 }
