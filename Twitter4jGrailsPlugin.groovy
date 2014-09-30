caf7b47ee8fd3a750caafe4578f6e2163842c8cd
Burt Beckwith <burt@burtbeckwith.com>
2014-09-29 22:43:43 -0400

    cleanup

--- Twitter4jGrailsPlugin.groovy
+++ Twitter4jGrailsPlugin.groovy
@@ -1,44 +1,32 @@
-import grails.util.Holders
 import org.twitter4j.grails.plugin.Twitter4jFactoryBean
 import org.twitter4j.grails.plugin.TwitterUserStreamFactoryBean
+
 import twitter4j.TwitterStream
 
 class Twitter4jGrailsPlugin {
 
-    // the plugin version (Follows Twitter4j library version + number for plugin revision)
-    def version = "4.0.4.0"
-    // the version or versions of Grails the plugin is designed for
+    def version = "4.0.4.0" // Follows Twitter4j library version + number for plugin revision
     def grailsVersion = "2.3 > *"
-    // resources that are excluded from plugin packaging
-    def pluginExcludes = [
-            "grails-app/views/error.gsp"
-    ]
-
     def title = "Twitter4j for Grails"
-    def author = "Soeren Berg Glasius"
-    def authorEmail = "soeren@glasius.dk"
     def description = 'Wraps the Twitter4j API by Groovy delegation (see http://www.twitter4j.org for API documentation and examples)'
-
     def documentation = "http://sbglasius.github.io/grails-twitter4j/"
-
     def license = "APACHE"
-
     def developers = [
-            [name: 'Arthur Neves', email: 'arthurnn@gmail.com'],
-            [name: "Daniel Gerbaudo", email: "info@danielgerbaudo.com"],
-            [name: "Rubén Salinas", email: "rubensalinasgarcia@gmail.com"],
-            [name: "Ricardo Vilella", email: "vilellaricardo@gmail.com"]
+        [name: 'Arthur Neves',        email: 'arthurnn@gmail.com'],
+        [name: "Daniel Gerbaudo",     email: "info@danielgerbaudo.com"],
+        [name: "Rubén Salinas",       email: "rubensalinasgarcia@gmail.com"],
+        [name: "Ricardo Vilella",     email: "vilellaricardo@gmail.com"],
+        [name: 'Soeren Berg Glasius', email: 'soeren@glasius.dk']
     ]
-
     def issueManagement = [system: "GitHub", url: "https://github.com/sbglasius/grails-twitter4j"]
     def scm = [url: "https://github.com/sbglasius/grails-twitter4j/settings"]
 
     def doWithSpring = {
-        Map twitterConfig = Holders.config.twitter4j
+        Map twitterConfig = application.config.twitter4j
 
         def d = configureBeans.clone()
         d.delegate = delegate
-        d.call(twitterConfig)
+        d(twitterConfig)
     }
 
     def doWithApplicationContext = { applicationContext ->
@@ -51,7 +39,7 @@ class Twitter4jGrailsPlugin {
         final c = configureBeans.clone()
         def beans = beans {
             c.delegate = delegate
-            c.call(grailsClass)
+            c(grailsClass)
         }
 
         beans.registerBeans(event.ctx)
@@ -60,30 +48,28 @@ class Twitter4jGrailsPlugin {
     def onShutdown = { event ->
         def ctx = event.ctx
         if (!ctx) {
-            log.error("Application context not found. Cannot execute shutdown code.")
+            log.error("ApplicationContext not found. Cannot execute shutdown code.")
             return
         }
-        def twitterStream = ctx.getBean("twitterStream")
-        if (twitterStream) {
-            twitterStream.shutdown()
-        }
+
+        ctx.twitterStream?.shutdown()
     }
 
     private scheduleStream = { ctx ->
-        if (ctx.containsBean('twitterUserListener')) {
-            def twitterUserListener = ctx.getBean('twitterUserListener')
-            TwitterStream twitterStream = ctx.getBean('twitterStream')
-            twitterStream.addListener(twitterUserListener);
-            twitterStream.user();
-
-            log.debug "Twitter User Stream Created.."
+        if (!ctx.containsBean('twitterUserListener')) {
+            return
         }
+
+        TwitterStream twitterStream = ctx.twitterStream
+        twitterStream.addListener ctx.twitterUserListener
+        twitterStream.user()
+
+        log.debug "Twitter User Stream Created.."
     }
 
     private configureBeans = { twitterConfig ->
         def userListenerClass = twitterConfig.userListenerClass
         if (userListenerClass) {
-
             def clazz = application.classLoader.loadClass(userListenerClass)
             log.debug "Register twitter user listenet[${clazz}]"
             twitterUserListener(clazz)
@@ -92,17 +78,17 @@ class Twitter4jGrailsPlugin {
         }
 
         def defaultConfig = twitterConfig.'default'
-        if(defaultConfig) {
-            twitterStream(TwitterUserStreamFactoryBean) {
-                configuration = defaultConfig
-            }
-
-            twitter4jService(Twitter4jFactoryBean) {
-                configuration = defaultConfig
-            }
-        } else {
+        if (!defaultConfig) {
             log.error("Remember to configure twitter4j")
+            return
+        }
+
+        twitterStream(TwitterUserStreamFactoryBean) {
+            configuration = defaultConfig
+        }
+
+        twitter4jService(Twitter4jFactoryBean) {
+            configuration = defaultConfig
         }
     }
-
 }
